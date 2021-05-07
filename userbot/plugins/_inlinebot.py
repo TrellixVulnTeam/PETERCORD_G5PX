a13fb0557fa273f66ad435a67b0cfb282b74bf33
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-07 11:30:20 +0700

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -7,9 +7,9 @@ import time
 
 from telethon import Button, custom, events
 
-from . import CMD_LIST, catalive
+from . import CMD_LIST, petercordalive
 
-CAT_IMG = Config.ALIVE_PIC or None
+PETERCORD_IMG = Config.ALIVE_PIC or None
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
@@ -21,30 +21,30 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         query = event.text
         hmm = re.compile("secret (.*) (.*)")
         match = re.findall(hmm, query)
-        if query.startswith("**Catuserbot") and event.query.user_id == bot.uid:
+        if query.startswith("**PETERCORD-USERBOT") and event.query.user_id == bot.uid:
             buttons = [
                 (
                     custom.Button.inline("Stats", data="stats"),
-                    Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
+                    Button.url("Repo", "https://github.com/ilham77mansiz/PETERCORD"),
                 )
             ]
-            if CAT_IMG and CAT_IMG.endswith((".jpg", ".png")):
+            if PETERCORD_IMG and PETERCORD_IMG.endswith((".jpg", ".png")):
                 result = builder.photo(
-                    CAT_IMG,
-                    # title="Alive cat",
+                    PETERCORD_IMG,
+                    # title="Alive PETERCORD",
                     text=query,
                     buttons=buttons,
                 )
-            elif CAT_IMG:
+            elif PETERCORD_IMG:
                 result = builder.document(
-                    CAT_IMG,
-                    title="Alive cat",
+                    PETERCORD_IMG,
+                    title="Alive PETERCORD",
                     text=query,
                     buttons=buttons,
                 )
             else:
                 result = builder.article(
-                    title="Alive cat",
+                    title="Alive PETERCORD",
                     text=query,
                     buttons=buttons,
                 )
@@ -139,7 +139,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
             ]
             result = builder.article(
                 title="secret message",
-                text=f"🔒 A whisper message to {sandy}, Only he/she can open it.",
+                text=f"🔒 A whisper message to {ilham}, Only he/she can open it.",
                 buttons=buttons,
             )
             await event.answer([result] if result else None)
@@ -178,7 +178,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @catuserbot17 help "
+            reply_pop_up_alert = "Please get your own catuserbot, and don't use mine! Join @petercord help "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.*)")))
